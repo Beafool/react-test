66f8c7022c3b4bfd6111fc9b79d28e7313a0beac
fxiong <1308694300@qq.com>
2019-04-03 20:07:15 +0800

    完成home组件和其他基本组件

--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -12,6 +12,12 @@ import Home from '../home';
 import Category from '../category';
 import Product from '../product';
 import LeftNav from '../../components/left-nav';
+import User from '../user';
+import Role from '../role';
+import Bar from '../charts/bar';
+import Line from '../charts/line';
+import Pie from '../charts/pie';
+
 import { getItem } from "../../utils/storage-utils";
 import memory from '../../utils/memory-utils';
 import HeaderMain from "../../components/header-main";
@@ -73,7 +79,11 @@ export default class Admin extends Component {
                                 <Route path="/home" component={Home} />
                                 <Route path="/category" component={Category} />
                                 <Route path="/product" component={Product} />
-
+                                <Route path="/user" component={User} />
+                                <Route path="/role" component={Role} />
+                                <Route path="/charts/bar" component={Bar}/>
+                                <Route path="/charts/line" component={Line}/>
+                                <Route path="/charts/pie" component={Pie}/>
                                 <Redirect to="/home" />
                             </Switch>
                         </div>
