b88ada8df299b1ed462f95f7b42126ee1cfe3391
fxiong <1308694300@qq.com>
2019-04-11 11:01:44 +0800

    '图表库的使用'

--- src/pages/charts/index.jsx
+++ src/pages/charts/index.jsx
@@ -1,18 +1,17 @@
-import React, {Component} from 'react';
-import { Route, Redirect ,Switch } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
+
 import Bar from "./bar";
-import Line from "./line";
 import Pie from "./pie";
+import Line from "./line";
 
 export default class Charts extends Component {
-  render () {
-    return (
-      <Switch>
-          <Route path="/charts/bar" component={Bar}/>
-          <Route path="/charts/line" component={Line}/>
-          <Route path="/charts/pie" component={Pie}/>
-          <Redirect to="/charts/bar" />
-      </Switch>
-    )
+  render() {
+    return <Switch>
+      <Route path="/charts/line" component={Line}/>
+      <Route path="/charts/bar" component={Bar}/>
+      <Route path="/charts/pie" component={Pie}/>
+      <Redirect to="/charts/bar"/>
+    </Switch>;
   }
 }
